48370b02b2e39e8e89fed0b9fa38d8e465abd368
anonymous <anonymous>
2022-12-08 22:14:50 +0100

    wip autopull images, create-if-not-exists for containers

--- src/solvents/main.clj
+++ src/solvents/main.clj
@@ -1,5 +1,7 @@
 (ns solvents.main
-  (:require [contajners.core :as c])
+  (:require [contajners.core :as c]
+            [clojure.data.json :as json]
+            [clojure.string :as str])
   (:gen-class))
 
 (def
@@ -19,13 +21,29 @@
         :containers
         :volumes]))))
 
+(defmacro contain [name data]
+  `(do
+     (~'gallery {:op :ImageCreate
+                 :params {:fromImage (:Image ~data)}})
+     
+     (when-not (~'exists [(str "/" ~name)] :Names (~'box {:op :ContainerList
+                                                          :params {:all true}}))
+       (~'box {:op :ContainerCreate
+               :params {:name ~name}
+               :data ~data
+               :throw-exceptions true}))
+     (~'box {:op :ContainerStart
+             :params {:id ~name}})))
+
+
 (def
   ^{}
   runner
   (fn []
     (let [client  (make-client)
-          boxes   (:containers client)
-          nets    (:networks client)
+          box     (partial c/invoke (:containers client))
+          net     (partial c/invoke (:networks client))
+          gallery (partial c/invoke (:images client))
           
           exists  (fn [e k l] (-> (comp #{e} k)
                                   (filter l)
@@ -33,29 +51,35 @@
                                   (> 0)))
           netname "acid"]
 
-      (when-not (exists netname :Name (c/invoke nets {:op :NetworkList}))
-        (c/invoke nets {:op :NetworkCreate
-                        :data {:CheckDuplicate true
-                               :Name netname}}))
+      (when-not (exists netname :Name (net {:op :NetworkList}))
+        (net {:op :NetworkCreate
+              :data {:CheckDuplicate true
+                     :Name netname}}))
 
-      (when-not (exists ["/solutes-api"] :Names (c/invoke boxes {:op :ContainerList
-                                                                 :params {:all true}}))
-        (c/invoke boxes {:op :ContainerCreate
-                         :params {:name "solutes-api"}
-                         :data {:Cmd ["/bin/sh" "-c" "while true; do sleep 10; done"]
-                                :Image "python:3.8-slim"
-                                :HostConfig {:binds [(format "%s:/tmp/b" (System/getProperty "user.dir"))]}
-                                :NetworkingConfig {:NetworkID "acid"}}
-                         :throw-exception true})
-        )
+      (contain "acid-solutes"
+               {:Image "acid/solutes"
+                :HostConfig {:binds [(format "%s:/tmp/b" (System/getProperty "user.dir"))]}
+                :NetworkingConfig {:NetworkID "acid"}
+                :ExposedPorts {"8000/tcp" "8000/tcp"}
+                })
 
-      (c/invoke boxes {:op :ContainerStart
-                       :params {:id "solutes-api"}})
+      (contain "acid-acidoc"
+               {:Image "swaggerapi/swagger-ui"
+                :NetworkingConfig {:NetworkID "acid"}
+                :Env [(format "API_URLS=%s" (json/write-str
+                                             {:url "http://localhost/openapi.json"
+                                              :name "test API doc"}))]})
 
-      )))
+      (map (fn [e] (:Names e)) (box {:op :ContainerList})))))
 
 
 (comment
+
+  (defmacro macro-eval [digit]
+    `(* ~'left ~digit))
+
+  (let [left 2]
+    (macro-eval 5))
 
   (runner)
   )
